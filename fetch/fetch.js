497137ffbd6cc215165e79a7cb01cd1d8fceb53b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-15 09:34:51 -0500

    Update fetch.js

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,3 +1,62 @@
+
+
+globalThis.NewBlob=function(text){
+  return HtmlService.createHtmlOutput(text).getAs('text/plain');
+}
+
+
+globalThis.NewHttpResponse=function(body,options){
+  let res = {};
+  body = `${body}`;
+  res.headers = options?.headers||{};
+  res.status = options?.status||200;
+  res.getAllHeaders=function(){
+	  return this.headers;
+  };
+  res.getHeaders=function(){
+	 let flatHeaders={};
+	 for (const h in res.headers) {
+		flatHeaders[h]=`${res.headers[h]}`;
+	}
+	return flatHeaders;
+  };
+  
+  res.body = body;
+  res.bodyBlob = NewBlob(body);
+  res.getContent = function(){
+	  return this.bodyBlob.getBytes();
+  };
+  res.getContentText = function(charset){
+	  if(!charset){
+		  return this.body;
+	  }
+	  return this.bodyBlob.getDataAsString(charset);
+  };
+  res.getResponseCode=function(){
+	return res.status;  
+  };
+  return res;
+}
+
+
 globalThis.UrlFetch = function(url,options){
-  return UrlFetchApp.fetch(url,{validateHttpsCertificates:false,muteHttpExceptions:true,escaping:false});
+  if(!options){
+    options = {};
+  }
+    options.validateHttpsCertificates = false;
+    options.mutHttpExceptions = true;
+    options.escaping = false;
+  return UrlFetchApp.fetch(url,options);
+}
+
+globalThis.zUrlFetch = function(url,options){
+	try{
+	  UrlFetchApp.fetch(url,options);
+	}catch(e){
+		return NewHttpResponse(`569 ${e.message}`,{status:569});
+	}
+}
+
+function test() {
+  console.log(UrlFetch('asdf'));
 }
