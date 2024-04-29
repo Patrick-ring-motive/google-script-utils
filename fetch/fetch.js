c32a7a78aa4ade74346e1cb37f5d40d91fd78023
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-29 15:06:39 -0500

    Update fetch.js

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,5 +1,7 @@
 /** ScriptID for Library import: 1wV9VS-iD51TTBseNrW6dzSF3l6x_gayn7ETOJIjx0YIAb4N_WhxLhED4 **/
 
+
+
 /** 
 * Creates a new HTTP response simulation. 
 * @param {string} body - The response body as a string. 
@@ -26,11 +28,17 @@ globalThis.NewHttpResponse = function(body, options) {
     res.getContent = function() {
         return this.bodyBlob.getBytes();
     };
+    res.getAs = function (type){
+      return this.bodyBlob.getAs(type);
+    }
     res.getContentText = function(charset) {
         if (!charset) {
             return this.body;
         }
         return this.bodyBlob.getDataAsString(charset);
+    };
+    res.toString = function(){
+      return this.body;
     };
     res.getResponseCode = function() {
         return res.status;
@@ -48,9 +56,9 @@ globalThis.UrlFetch = function(url, options) {
     if (!options) {
         options = {};
     }
-    options.validateHttpsCertificates = false;
-    options.muteHttpExceptions = true;
-    options.escaping = false;
+    options.validateHttpsCertificates = options.validateHttpsCertificates ?? false;
+    options.muteHttpExceptions = options.muteHttpExceptions ?? true;
+    options.escaping = options.escaping ?? false;
     return UrlFetchApp.fetch(url, options);
 }
 
@@ -62,7 +70,7 @@ globalThis.UrlFetch = function(url, options) {
  */
 globalThis.zUrlFetch = function(url, options) {
     try {
-        return UrlFetchApp.fetch(url, options);
+        return UrlFetch(url, options);
     } catch (e) {
         return NewHttpResponse(`569 ${e.message}`, {
             status: 569
@@ -70,6 +78,91 @@ globalThis.zUrlFetch = function(url, options) {
     }
 }
 
-function test() {
-    console.log(zUrlFetch('asdf').getContentText());
+
+globalThis.NewHttpRequest = function(url, options) {
+    if (!options) {
+        options = {};
+    }
+    options.validateHttpsCertificates = options.validateHttpsCertificates ?? false;
+    options.muteHttpExceptions = options.muteHttpExceptions ?? true;
+    options.escaping = options.escaping ?? false;
+    let req = UrlFetchApp.getRequest(url, options); 
+    req.validateHttpsCertificates = options.validateHttpsCertificates ?? false;
+    req.muteHttpExceptions = options.muteHttpExceptions ?? true;
+    req.escaping = options.escaping ?? false;
+  return req;
+}
+
+
+globalThis.zNewHttpRequest = function(url, options) {
+    if (!options) {
+        options = {};
+    }
+    options.validateHttpsCertificates = options.validateHttpsCertificates ?? false;
+    options.muteHttpExceptions = options.muteHttpExceptions ?? true;
+    options.escaping = options.escaping ?? false;
+    console.log(options);
+    let req = {}
+    try{
+      req = UrlFetchApp.getRequest(url, options); 
+    }catch(e){
+      req = UrlFetchApp.getRequest('https://www.google.com', options); 
+      req.url = url;
+    }
+    try{delete req.headers['X-Forwarded-For']}catch(e){}
+    req.validateHttpsCertificates = options.validateHttpsCertificates ?? false;
+    req.muteHttpExceptions = options.muteHttpExceptions ?? true;
+    req.escaping = options.escaping ?? false;
+  return req;
+}
+
+globalThis.UrlFetchAll=function(requests){
+  for(let i = 0;i<requests.length;i++){
+      requests[i] = NewHttpRequest(requests[i].url,requests[i]);
+      delete requests[i].headers['X-Forwarded-For'];
+  }
+  return UrlFetchApp.fetchAll(requests);
+}
+
+globalThis.zUrlFetchAllSync=function(requests){
+  let responses = [];
+    for(let i = 0;i<requests.length;i++){
+      let req = zNewHttpRequest(requests[i].url,requests[i]);
+        responses.push(zUrlFetch(req.url,req));
+    }
+    return responses;
+}
+
+globalThis.zUrlFetchAll=function(requests){
+  try{
+    return UrlFetchAll(requests);
+  }catch(e){
+    return zUrlFetchAllSync(requests);
+  }
+}
+
+globalThis.HttpEvent=function(e){
+  e = e ?? {};
+  e.queryString = e.queryString ?? '';
+  e.parameter = e.parameter ?? {};
+  e.parameters = e.parameters ?? {};
+  e.pathInfo = e.pathInfo ?? '';
+  e.contextPath = contextPath ?? '';
+  e.postData = e.postData ?? {};
+  e.postData.contents = e.postData.contents ?? '';
+  e.postData.length = e.postData.length ?? e.postData.contents.length;
+  e.contentLength = e.contentLength ?? e.postData.length;
+  e.postData.type = e.postData.type ?? "text/plain";
+  e.postData.name = e.postData.name ?? "postData";
+  return e;
+}
+
+function test(e) {
+  let req = NewHttpRequest('https://www.google.com');
+  console.log(req)
+  let req2 = zNewHttpRequest('asdf');
+  delete req.headers['X-Forwarded-For'];
+    console.log(zUrlFetchAll([req,req2]));
+   // console.log(zUrlFetch('https://www.google.com'));
+    // console.log(zUrlFetch('asdf'))
 }
